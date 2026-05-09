ff13606e925571be9c99b43da18d82713f85a29f
manuel <manuel@MacBook-Air-von-manuel.local>
2026-05-09 17:37:05 +0200

    switch to OAuth 1.0a authentication

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,40 +1,18 @@
 import NextAuth from "next-auth";
+import TwitterProvider from "next-auth/providers/twitter";
 
 const authOptions = {
   providers: [
-    {
-      id: "twitter",
-      name: "X",
-      type: "oauth",
-      version: "2.0",
+    TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID!,
       clientSecret: process.env.TWITTER_CLIENT_SECRET!,
-      authorization: {
-        url: "https://x.com/i/oauth2/authorize",
-        params: {
-          scope: "users.read tweet.read offline.access",
-        },
-      },
-      token: "https://api.x.com/2/oauth2/token",
-      userinfo: {
-        url: "https://api.x.com/2/users/me",
-        params: { "user.fields": "username,profile_image_url" },
-      },
-      checks: ["pkce", "state"],
-      profile(profile: any) {
-        return {
-          id: profile.data.id,
-          name: profile.data.name,
-          email: null,
-          image: profile.data.profile_image_url,
-        };
-      },
-    },
+      version: "1.0a",
+    }),
   ],
   callbacks: {
     async jwt({ token, account, profile }: any) {
       if (account && profile) {
-        token.xHandle = profile?.data?.username ?? null;
+        token.xHandle = profile?.screen_name ?? null;
       }
       return token;
     },
@@ -46,5 +24,5 @@ const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 };
 
-const handler = NextAuth(authOptions as any);
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
